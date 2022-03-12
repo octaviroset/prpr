c3519b9b3d166b64a00c5b2c5988130e2db72983
sol <sol.github@gmail.com>
2022-03-12 18:09:37 +0100

    stage context

--- Main.java
+++ Main.java
@@ -1,10 +1,19 @@
 import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) {
+        Scene scene = new Scene(createContext());
+        stage.setScene(scene);
+        stage.show();
+    }
 
+    private Parent createContext() {
+        return new Pane();
     }
 
     public static void main(String[] args) {
